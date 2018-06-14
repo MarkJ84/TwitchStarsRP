bea9c0c2687feb3a17da00ccfe89243cfecd7130
MarkJ84 <40249838+MarkJ84@users.noreply.github.com>
2018-06-14 17:00:45 +0100

    Updated Starter Scripts
    Slash before run . cmd facing the wrong way

--- Server 1/Server1Starter.cmd
+++ Server 1/Server1Starter.cmd
@@ -1,2 +1,2 @@
 cd /d C:\Users\Admin\Desktop\server\cfx-server-data
-C:\Users\Admin\Desktop\server/run.cmd +exec server.cfg
+C:\Users\Admin\Desktop\server\run.cmd +exec server.cfg
--- Server 2/Server2Starter.cmd
+++ Server 2/Server2Starter.cmd
@@ -1,2 +1,2 @@
 cd /d C:\Users\Admin\Desktop\server2\cfx-server-data
-C:\Users\Admin\Desktop\server2/run.cmd +exec server.cfg
+C:\Users\Admin\Desktop\server2\run.cmd +exec server.cfg
