1268d9138706f2a7a6893bda4d4dcaa7ab59471a
MarkJ84 <40249838+MarkJ84@users.noreply.github.com>
2018-06-23 20:45:23 +0100

    #1 Spawnpoints Updated

--- Server 1/cfx-server-data/resources/fivem-map-hipster/map.lua
+++ Server 1/cfx-server-data/resources/fivem-map-hipster/map.lua
@@ -1,6 +1,7 @@
 vehicle_generator "airtug" { -54.26639938354492, -1679.548828125, 28.4414, heading = 228.2736053466797 }
 
 spawnpoint 'a_m_y_hipster_02' {x = -207, y = -1014, z = 30}
+spawnpoint 'a_m_y_hipster_01' {x = -207, y = -1014, z = 30}
 --spawnpoint 'a_m_y_hipster_01' { x = -802.311, y = 175.056, z = 72.8446 }
 --spawnpoint 'a_m_y_hipster_02' { x = -9.96562, y = -1438.54, z = 31.1015 }
 --spawnpoint 'a_m_y_hipster_01' { x = 0.916756, y = 528.485, z = 174.628 }
